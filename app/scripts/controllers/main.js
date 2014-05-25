9ceccef78db2b9806b95c211f428f13ce1643c4f
kwakwak <ronisM3>
2014-05-25 13:54:28 +0300

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('casesApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
